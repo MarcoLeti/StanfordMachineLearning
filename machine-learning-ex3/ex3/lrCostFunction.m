0b9993d325a65b9d13be601f46d3f18cc7030f41
MarcoLeti <letico.marco@gmail.com>
2018-03-11 19:54:28 +0100

    regularized cost function

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,12 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = (1/m * ((log(sigmoid(X * theta))' * -y) ... 
+    - log(1 - sigmoid(X * theta))' * (1 - y))) ...
+    + lambda/(2*m) * sum(theta(2:end) .^ 2);
 
-
-
-
-
-
+grad = 1/m * X' * (sigmoid(X * theta) - y) ...
+        + [0; (lambda/m) .* theta(2:end)];
 
 
 
